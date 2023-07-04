9d675284a0ac14dfa958ffc87198f854ae990f49
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-07-04 17:59:31 +0100

    ts2 progression

---
+++ Ts2/Functions.js
@@ -0,0 +1,6 @@
+function getName(user){
+ return `My name is ${user.last} ${user.first} I am ${user.age} years old`
+}
+console.log(getName({first:'abdull', last:'usman', age:400}))
+
+// I actually haven't try if passing object as a parameter in javaScript is possible but got to know more today, it's really amazing to learnign TypeScript
--- Ts2/function.ts
+++ Ts2/function.ts
@@ -16,3 +16,23 @@ export const printFormat = (title: string, param: string | number): void => {
  console.log(format(title, param))
 }
 
+
+printFormat('usman',80)
+
+export const fetchData = (url: string)=> 
+Promise.resolve(`Data from ${url}`)
+
+function introduce(salutation: string, ...names: string[]): string{
+ return `${salutation} ${names.join(' ')}`;
+}
+const names2 = 'abdullahi'
+const names3 = 'abdullahi3'
+console.log(introduce('welcome',names2,names3))
+
+export function getName(user:{firstName: string, lastName: string, age: number}): string{
+ return `My name is ${user.firstName} ${user.lastName} I am ${user.age} years old`
+}
+
+console.log(getName({firstName:'usman', lastName:'abdullahi', age:30}))
+
+// JavaScript is not checking for errors at runtime only does it at compiled time, while typescript checks for errors at runtime
