a5a36119bceecb5944a85c42ca2e832d271db5cb
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-07-11 17:42:39 +0100

    fixes

--- Challenge1/Solution.ts
+++ Challenge1/Solution.ts
@@ -15,17 +15,18 @@ interface HouseWithID extends House{
  id: number;
 }
 
-function findHouses(houses: string): HouseWithID[];
-function findHouses(
- houses: string,
- filter: (house: House)=> boolean
-): HouseWithID[];
+// function findHouses(houses: string): HouseWithID[];
+// function findHouses(
+//  houses: string,
+//  filter: (house: House)=> boolean
+// ): HouseWithID[];
 
-function findHouses(houses: House[]): HouseWithID[];
-function findHouses(
- houses: House[],
- filter: (house: House)=> boolean
-): HouseWithID[];
+// function findHouses(houses: string | House[]): HouseWithID[];
+// function findHouses(
+//  houses: string | House[],
+//  filter: (house: House)=> boolean
+// ): HouseWithID[];
+
 function findHouses(
  input: string | House[],
  filter?: (house: House)=> boolean
